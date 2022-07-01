9155662bd2ca8c14b90db7f4c00b4aba13336755
Joey Wong <370318843@qq.com>
2022-07-01 23:20:15 +0800

    01tut

---
+++ 01tut/server.js
@@ -0,0 +1,18 @@
+// const os = require("os");
+
+// console.log(global);
+
+// console.log(os.type());
+// console.log(os.version());
+// console.log(os.homedir());
+
+// console.log(__dirname);
+
+// const path = require("path");
+// console.log(path.basename(__filename));
+// console.log(path.extname(__filename));
+// console.log(path.parse(__filename));
+
+const { multiply } = require("./math");
+
+console.log(multiply(2, 3));
