0732f1789b45ebe436a2352159cf2f886826caa3
Joey Wong <370318843@qq.com>
2022-07-02 09:36:25 +0800

    avoid callback hell with fs promises

--- 02tut/file-system.js
+++ 02tut/file-system.js
@@ -1,19 +1,21 @@
-const fs = require("fs");
-const path = require("path");
+const fs = require('fs')
+const path = require('path')
+/*
 fs.readFile(
-  path.join(__dirname, "files", "starter.txt"),
-  "utf-8",
+  path.join(__dirname, 'files', 'starter.txt'),
+  'utf-8',
   (err, data) => {
-    if (err) throw err;
-    console.log(data);
+    if (err) throw err
+    console.log(data)
   }
-);
-console.log("print first");
+)
+console.log('print first')
 
-process.on("uncaughtException", (err) => {
-  console.error(`There was an uncaught error: ${err}`);
-  process.exit(1);
-});
+process.on('uncaughtException', (err) => {
+  console.error(`There was an uncaught error: ${err}`)
+  process.exit(1)
+})
+*/
 
 /*
 callback hell
@@ -42,3 +44,38 @@ fs.writeFile(
   }
 );
 */
+
+const fsPromsies = require('fs').promises
+
+const fileOps = async () => {
+  try {
+    const data = await fsPromsies.readFile(
+      path.join(__dirname, 'files', 'starter.txt'),
+      'utf8'
+    )
+    console.log(data)
+    await fsPromsies.writeFile(
+      path.join(__dirname, 'files', 'promiseWrite.txt'),
+      data
+    )
+    await fsPromsies.appendFile(
+      path.join(__dirname, 'files', 'promiseWrite.txt'),
+      `\n\nNice to meet you`
+    )
+    await fsPromsies.rename(
+      path.join(__dirname, 'files', 'promiseWrite.txt'),
+      path.join(__dirname, 'files', 'promiseWriteComplete.txt')
+    )
+    const newData = await fsPromsies.readFile(
+      path.join(__dirname, 'files', 'promiseWriteComplete.txt'),
+      'utf8'
+    )
+    console.log(newData)
+    fsPromsies.unlink(
+      path.join(__dirname, 'files', 'promiseWriteComplete.txt')
+    )
+  } catch (error) {
+    console.log(error)
+  }
+}
+fileOps()
