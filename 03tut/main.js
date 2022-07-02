1aacd6094de4cc2aafe01d2f8d09fe6130cd6467
Joey Wong <370318843@qq.com>
2022-07-02 16:56:32 +0800

    event emitter

--- 03tut/main.js
+++ 03tut/main.js
@@ -1,1 +1,15 @@
-console.log('testing');
+const logEvents = require('./logEvents')
+
+const EventEmitter = require('events')
+
+class MyEmitter extends EventEmitter {}
+
+// initialize object
+const myEmitter = new MyEmitter()
+
+//  add listener for the log event
+myEmitter.on('customLog', (msg) => {
+  logEvents(msg)
+})
+
+myEmitter.emit('customLog', 'Log event emitted')
